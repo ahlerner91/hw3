4a5fcfc825f1b1b680ef9bc183f3ba4b9a2a8c54
Alison Lerner <ahlerner@gmail.com>
2021-01-25 12:19:48 -0600

    Updates to passenger and level of service

--- noober.js
+++ noober.js
@@ -11,13 +11,26 @@ function pageLoad() {
 
     if (ride.length > 1) {
     levelOfService = 'Noober Pool'
-  } else if (ride[0].purpleRequested = 'true') {
-    levelOfService = 'Noober Purple' 
   } else if (ride[0].numberOfPassengers > 3) {
     levelOfService = 'Noober XL'
+  } else if (ride[0].purpleRequested = true) {
+    levelOfService = 'Noober Purple' 
   } else {levelOfService = 'Noober X'
   }
 
+  console.log(levelOfService)
+
+  if(ride.length == 1) {
+  passenger1Name = ride[0].passengerDetails.first + ' ' + ride[0].passengerDetails.last
+  passenger1Phone = ride[0].passengerDetails.phoneNumber
+  passenger1NumberOfPassengers = ride[0].numberOfPassengers
+  passenger1PickupAddressLine1 = ride[0].pickupLocation.address
+  passenger1PickupAddressLine2 = ride[0].pickupLocation.city + ', ' + ride[0].pickupLocation.state + ' ' + ride[0].pickupLocation.zip
+  passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
+  passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + ', ' + ride[0].dropoffLocation.state + ' ' + ride[0].dropoffLocation.zip
+  }
+
+  else if (ride.length == 2) {
 
   passenger1Name = ride[0].passengerDetails.first + ' ' + ride[0].passengerDetails.last
   passenger1Phone = ride[0].passengerDetails.phoneNumber
@@ -26,10 +39,7 @@ function pageLoad() {
   passenger1PickupAddressLine2 = ride[0].pickupLocation.city + ', ' + ride[0].pickupLocation.state + ' ' + ride[0].pickupLocation.zip
   passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
   passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + ', ' + ride[0].dropoffLocation.state + ' ' + ride[0].dropoffLocation.zip
-
-
-  
-
+    
   passenger2Name = ride[1].passengerDetails.first + ' ' + ride[1].passengerDetails.last
   passenger2Phone = ride[1].passengerDetails.phoneNumber
   passenger2NumberOfPassengers = ride[1].numberOfPassengers
@@ -37,8 +47,26 @@ function pageLoad() {
   passenger2PickupAddressLine2 = ride[1].pickupLocation.city + ', ' + ride[1].pickupLocation.state + ' ' + ride[1].pickupLocation.zip
   passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
   passenger2DropoffAddressLine2 = ride[1].dropoffLocation.city + ', ' + ride[1].dropoffLocation.state + ' ' + ride[1].dropoffLocation.zip
+  }
 
-
+else if (ride.length == 3) {
+  
+  passenger1Name = ride[0].passengerDetails.first + ' ' + ride[0].passengerDetails.last
+  passenger1Phone = ride[0].passengerDetails.phoneNumber
+  passenger1NumberOfPassengers = ride[0].numberOfPassengers
+  passenger1PickupAddressLine1 = ride[0].pickupLocation.address
+  passenger1PickupAddressLine2 = ride[0].pickupLocation.city + ', ' + ride[0].pickupLocation.state + ' ' + ride[0].pickupLocation.zip
+  passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
+  passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + ', ' + ride[0].dropoffLocation.state + ' ' + ride[0].dropoffLocation.zip
+    
+  passenger2Name = ride[1].passengerDetails.first + ' ' + ride[1].passengerDetails.last
+  passenger2Phone = ride[1].passengerDetails.phoneNumber
+  passenger2NumberOfPassengers = ride[1].numberOfPassengers
+  passenger2PickupAddressLine1 = ride[1].pickupLocation.address
+  passenger2PickupAddressLine2 = ride[1].pickupLocation.city + ', ' + ride[1].pickupLocation.state + ' ' + ride[1].pickupLocation.zip
+  passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
+  passenger2DropoffAddressLine2 = ride[1].dropoffLocation.city + ', ' + ride[1].dropoffLocation.state + ' ' + ride[1].dropoffLocation.zip
+  
   passenger3Name = ride[2].passengerDetails.first + ' ' + ride[2].passengerDetails.last
   passenger3Phone = ride[2].passengerDetails.phoneNumber
   passenger3NumberOfPassengers = ride[2].numberOfPassengers
@@ -46,6 +74,10 @@ function pageLoad() {
   passenger3PickupAddressLine2 = ride[2].pickupLocation.city + ', ' + ride[2].pickupLocation.state + ' ' + ride[2].pickupLocation.zip
   passenger3DropoffAddressLine1 = ride[2].dropoffLocation.address
   passenger3DropoffAddressLine2 = ride[2].dropoffLocation.city + ', ' + ride[2].dropoffLocation.state + ' ' + ride[2].dropoffLocation.zip
+}
+
+
+
 
   // these variables map to the elements on the finished page;
   // assign the proper value to each variable
